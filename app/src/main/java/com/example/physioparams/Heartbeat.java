63f2dce09b64f7811d1237f9835c3509d16d5bdd
Shaktiraj Daudra <shaktirajdaudra@gmail.com>
2021-04-07 15:45:25 +0530

    Init

---
+++ app/src/main/java/com/example/physioparams/Heartbeat.java
@@ -0,0 +1,14 @@
+package com.example.physioparams;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Heartbeat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_heartbeat);
+    }
+}
---
+++ app/src/main/java/com/example/physioparams/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.physioparams;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final Button mHeartbeatButton = findViewById(R.id.heartbeat);
+        mHeartbeatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Heartbeat.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
